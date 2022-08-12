fea77485b6cda9f79f06fc5657f6f702783024ef
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2022-08-12 23:09:52 +0900

    상품 추가 기능, 상품 목록 기능 추가

---
+++ src/main/java/com/login/loginAPI/domain/Item.java
@@ -0,0 +1,42 @@
+package com.login.loginAPI.domain;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "item")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long itemID;
+
+    private String name;
+
+    private Integer price;
+
+    private Integer register;
+
+    private Integer volume;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    public Item(String name, Integer price, Integer register) {
+        this.name = name;
+        this.price = price;
+        this.register = register;
+    }
+
+    public Item(){}
+}
