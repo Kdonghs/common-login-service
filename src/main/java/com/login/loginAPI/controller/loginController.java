841cb87d739728fc44c2debe45c18ae7533c1a61
김동혁 <kkk951744@naver.com>
2022-08-16 15:57:09 +0900

    session add(complete yet)

--- src/main/java/com/login/loginAPI/controller/loginController.java
+++ src/main/java/com/login/loginAPI/controller/loginController.java
@@ -6,7 +6,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/login")
@@ -15,16 +21,27 @@ public class loginController {
 
     @Autowired
     private MemberService memberService;
+    sessionController session = new sessionController();
 
     @RequestMapping("/login")
-    public String login(Member member, Model model){
+    public String login(Member member, BindingResult bindingResult, HttpServletResponse response){
 
         System.out.println("member.get() = " + member.getSocial());
         String flag = memberService.loginOk(member.getId(),member.getPassword());
-        if (flag!=""){
-            model.addAttribute("name",flag + " 로그인됨");
+        if(flag==null){
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        } else {
+            session.createSession(member,response);
+            return "home";
         }
-        return "home";
     }
+
+    @RequestMapping("/logOut")
+    public String logoutV2(HttpServletRequest request) {
+        session.expires(request);
+        return "login/loginForm";
+    }
+
 
 }
